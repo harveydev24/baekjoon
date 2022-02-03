6f6950145a2fc49c2f0a129d15337535c7a2a192
Jinhwan Kim <97587300+harveydev24@users.noreply.github.com>
2022-02-03 21:38:49 +0900

    Update 아기 상어.py

--- 백준/Gold/16236.아기 상어/아기 상어.py
+++ 백준/Gold/16236.아기 상어/아기 상어.py
@@ -21,7 +21,7 @@ dx, dy = [1,-1,0,0],[0,0,1,-1]
 def bfs(x,y):
     global array,t,shark
     visited = [[False]*N for _ in range(N)]
-    q = deque([(shark['pos'][0], shark['pos'][1], 1)])
+    q = deque([(shark['pos'][0], shark['pos'][1], 0)])
     visited[shark['pos'][0]][shark['pos'][1]] = True
     t_min = float('inf')
     fish_lst = []
@@ -37,10 +37,12 @@ def bfs(x,y):
                     q.append((xx,yy,curr[2]+1))
                     visited[xx][yy] = True
                     if 0<array[xx][yy]<shark['size']:
-                        t_min = curr[2]
-                        fish_lst.append((xx,yy))
+                        if t_min >= curr[2]+1:
+                            t_min = curr[2]+1
+                            fish_lst.append((xx,yy))
     
     fish_lst.sort()
+
     if not fish_lst:
         print(t)
         exit(0)
@@ -48,11 +50,12 @@ def bfs(x,y):
         array[shark['pos'][0]][shark['pos'][1]] = 0
         shark['pos'] = fish_lst[0]
         t += t_min
-        array[fish_lst[0][0]][fish_lst[0][1]] = 0
+        array[fish_lst[0][0]][fish_lst[0][1]] = 9
         shark['fish'] += 1
         if shark['fish'] == shark['size']:
             shark['fish'] = 0
             shark['size'] += 1
+    
 
 
 while True:
