2f74aa4bdc10fadec7b48a5a82c9af316496ffad
Jinhwan Kim <97587300+harveydev24@users.noreply.github.com>
2022-02-28 22:00:00 +0900

    [Gold IV] Title: 플로이드, Time: 372 ms, Memory: 128600 KB -BaekjoonHub

---
+++ 백준/Gold/11404.플로이드/플로이드.py
@@ -0,0 +1,20 @@
+n = int(input())
+m = int(input())
+cost = [[float('inf')]*(1+n) for _ in range(1+n)]
+
+for _ in range(m):
+    a, b, c = map(int, input().split())
+    cost[a][b] = min(cost[a][b], c)
+
+for k in range(1, n+1):
+    for i in range(1, n+1):
+        for j in range(1, n+1):
+            cost[i][j] = min(cost[i][j], cost[i][k] + cost[k][j])
+
+for i in range(1,n+1):
+    for j in range(1,n+1):
+        if cost[i][j] == float('inf') or j == i:
+            print(0, end=" ")
+        else:
+            print(cost[i][j], end=" ")
+    print()
