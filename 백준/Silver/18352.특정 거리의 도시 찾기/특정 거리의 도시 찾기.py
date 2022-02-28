10c81d788dbca8e36f0d918cca892452198df10a
Jinhwan Kim <97587300+harveydev24@users.noreply.github.com>
2022-02-28 21:52:39 +0900

    [Silver II] Title: 특정 거리의 도시 찾기, Time: 2084 ms, Memory: 181688 KB -BaekjoonHub

--- 백준/Silver/18352.특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
+++ 백준/Silver/18352.특정 거리의 도시 찾기/특정 거리의 도시 찾기.py
@@ -1,3 +1,4 @@
+# Dijstra
 from collections import deque
 
 N, M, K, X = map(int, input().split())
@@ -7,24 +8,25 @@ for _ in range(M):
     start, end = map(int, input().split())
     adj[start].append(end)
 
-dist = [-1] * (N+1) 
+dist = [float('inf')] * (N+1) 
 
-def bfs(X):
-    q = deque([(X,0)])
-    visited = [False] * (N+1)
-    visited[X] = True
-    isThere = 0
+def dijstra(X):
+    q = deque([(X, 0)])
+    dist[X] = 0 
+    isThere = False
 
     while q:
-        curr, cost = q.popleft()
+        curr_node, curr_dist = q.popleft()
+        if curr_dist > dist[curr_node]:
+            continue
 
-        for next in adj[curr]:
-            if not visited[next]:
-                dist[next] = cost + 1
-                visited[next] = True
-                q.append((next, cost+1))
-    
-    for i in range(1,N+1):
+        for next in adj[curr_node]:
+            tmp_dist = curr_dist + 1
+            if dist[next] > tmp_dist:
+                dist[next] = tmp_dist
+                q.append((next, tmp_dist))
+
+    for i in range(1, N+1):
         if dist[i] == K:
             print(i)
             isThere = True
@@ -32,4 +34,4 @@ def bfs(X):
     if not isThere:
         print(-1)
 
-bfs(X)
+dijstra(X)
