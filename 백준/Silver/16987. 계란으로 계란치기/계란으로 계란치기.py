b12993605a9bbfb0116330407c52ba7c152ba74b
Jinhwan Kim <97587300+harveydev24@users.noreply.github.com>
2022-04-06 14:04:15 +0900

    [Silver I] Title: 계란으로 계란치기, Time: 840 ms, Memory: 144392 KB -BaekjoonHub

---
+++ 백준/Silver/16987. 계란으로 계란치기/계란으로 계란치기.py
@@ -0,0 +1,41 @@
+N = int(input())
+eggs = []
+ans = 0
+
+
+def solve(idx, cnt):
+    global ans
+    if idx == N:
+        ans = max(ans, cnt)
+        return
+
+    if eggs[idx][0] <= 0:
+        solve(idx+1, cnt)
+        return
+
+    for i in range(N):
+        if i == idx:
+            continue
+
+        if eggs[i][0] > 0:
+            eggs[i][0] -= eggs[idx][1]
+            eggs[idx][0] -= eggs[i][1]
+            if eggs[i][0] <= 0 and eggs[idx][0] <= 0:
+                solve(idx+1, cnt+2)
+            elif eggs[i][0] <= 0 or eggs[idx][0] <= 0:
+                solve(idx+1, cnt+1)
+            else:
+                solve(idx+1, cnt)
+            eggs[i][0] += eggs[idx][1]
+            eggs[idx][0] += eggs[i][1]
+        else:
+            solve(idx + 1, cnt)
+
+
+for _ in range(N):
+    S, W = map(int, input().split())
+    eggs.append([S, W])
+
+solve(0, 0)
+
+print(ans)
