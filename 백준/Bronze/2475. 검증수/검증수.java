5d1fe7049f0f5166e2450de0c3d3bc1f3785eba0
Jinhwan Kim <97587300+harveydev24@users.noreply.github.com>
2022-04-08 21:21:53 +0900

    [Bronze V] Title: 검증수, Time: 204 ms, Memory: 17592 KB -BaekjoonHub

---
+++ 백준/Bronze/2475. 검증수/검증수.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int sum = 0;
+        for (int i=0; i<5; i++) {
+            int tmp = sc.nextInt();
+            sum += tmp*tmp;
+        }
+        System.out.println(sum%10);
+    }
+}
