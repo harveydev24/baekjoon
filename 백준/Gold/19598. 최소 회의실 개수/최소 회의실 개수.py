2fe1c977d3e48b23302476f3763cf8e58fbd2c2d
Jinhwan Kim <97587300+harveydev24@users.noreply.github.com>
2022-04-10 20:07:25 +0900

    [Gold V] Title: 최소 회의실 개수, Time: 512 ms, Memory: 131896 KB -BaekjoonHub

---
+++ 백준/Gold/19598. 최소 회의실 개수/최소 회의실 개수.py
@@ -0,0 +1,25 @@
+import sys
+import heapq
+input = sys.stdin.readline
+
+N = int(input())
+lst = []
+for _ in range(N):
+    lst.append(list(map(int, input().split())))
+
+lst.sort()
+cnt = 0
+
+q = [lst[0][1]]
+cnt = 1
+ans = 0
+
+for start, end in lst[1:]:
+    if start < q[0]:
+        cnt += 1
+    else:
+        heapq.heappop(q)
+    heapq.heappush(q, end)
+    ans = max(cnt, ans)
+
+print(ans)
