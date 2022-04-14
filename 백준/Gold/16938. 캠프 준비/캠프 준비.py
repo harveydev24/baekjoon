ee338ed1f1ba27f3b8ce7d923761004bd1fcf7bc
Jinhwan Kim <97587300+harveydev24@users.noreply.github.com>
2022-04-14 20:35:27 +0900

    [Gold V] Title: 캠프 준비, Time: 152 ms, Memory: 115372 KB -BaekjoonHub

--- 백준/Gold/16938. 캠프 준비/캠프 준비.py
+++ 백준/Gold/16938. 캠프 준비/캠프 준비.py
@@ -2,29 +2,17 @@ N, L, R, X = map(int, input().split())
 A = list(map(int, input().split()))
 ans = 0
 
-lst = [0]*N
+for i in range(1 << N):
+    tmp_sum = 0
+    tmp_max, tmp_min = 0, 10**6
 
+    for j in range(N):
+        if i & (1 << j):
+            tmp_max = max(tmp_max, A[j])
+            tmp_min = min(tmp_min, A[j])
+            tmp_sum += A[j]
 
-def solve(idx, cnt):
-    global ans
-    if idx == N:
-        if cnt >= 2:
-            tmp_max, tmp_min = 0, 10**6
-            tmp_sum = 0
-            for i in range(N):
-                if lst[i] == 1:
-                    tmp_max = max(tmp_max, A[i])
-                    tmp_min = min(tmp_min, A[i])
-                    tmp_sum += A[i]
-            if L <= tmp_sum <= R and X <= tmp_max-tmp_min:
-                ans += 1
-        return
+    if L <= tmp_sum <= R and X <= tmp_max-tmp_min:
+        ans += 1
 
-    lst[idx] = 1
-    solve(idx+1, cnt+1)
-    lst[idx] = 0
-    solve(idx+1, cnt)
-
-
-solve(0, 0)
 print(ans)
