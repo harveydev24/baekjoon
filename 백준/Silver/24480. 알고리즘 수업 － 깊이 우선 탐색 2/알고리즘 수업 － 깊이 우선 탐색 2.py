97a0a15dcf882b7e63d6c01b061f12d301d09638
Jinhwan Kim <97587300+harveydev24@users.noreply.github.com>
2022-05-27 23:33:55 +0900

    [Silver II] Title: 알고리즘 수업 - 깊이 우선 탐색 2, Time: 1424 ms, Memory: 733392 KB -BaekjoonHub

---
+++ 백준/Silver/24480. 알고리즘 수업 － 깊이 우선 탐색 2/알고리즘 수업 － 깊이 우선 탐색 2.py
@@ -0,0 +1,33 @@
+import sys
+sys.setrecursionlimit(10**5)
+input = sys.stdin.readline
+
+N, M, R = map(int, input().split())
+
+adj = [[] for _ in range(N+1)]
+
+for _ in range(M):
+    u, v = map(int, input().split())
+    adj[u].append(v)
+    adj[v].append(u)
+
+for i in range(1, N+1):
+    adj[i].sort(reverse=True)
+
+visited = [0]*(N+1)
+cnt = 1
+
+
+def dfs(i):
+    global cnt
+    visited[i] = cnt
+    cnt += 1
+    for next in adj[i]:
+        if not visited[next]:
+            dfs(next)
+
+
+dfs(R)
+
+for i in range(1, N+1):
+    print(visited[i])
